deddf255a1269352adce9bc04dd31f8a107a2e9f
Aaron Mitchell <amitchell@cogolabs.com>
2016-12-25 20:24:37 -0500

    adding saved print section, results are local only

--- print.js
+++ print.js
@@ -3,11 +3,17 @@ var fs = require('fs');
 
 var url =  'http://www.masslottery.com/data/json/search/dailygames/todays/15.json'
 
+
+var currentDate = new Date().toISOString().replace(/T|-/g, '_');
+var currentDate = currentDate.substring(0, currentDate.indexOf('.'));
+
+
 request(url, function (error, response, body) {
   if (!error && response.statusCode == 200) {
     var results = body;
     console.log(results.length)
     printFile('keno.json', results)
+    printFile('results/' + currentDate + '_results.json', results)
   }
 })
 
